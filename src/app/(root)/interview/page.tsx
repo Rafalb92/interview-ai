62b33b1d4639fc351e5ce218c193c2822735fce1
Rafalb92 <bochniewiczrafal@gmail.com>
2025-03-26 15:22:05 +0100

    update agent for interviews tests

--- src/app/(root)/interview/page.tsx
+++ src/app/(root)/interview/page.tsx
@@ -1,51 +1,25 @@
-import Image from 'next/image';
-import { redirect } from 'next/navigation';
-
 import Agent from '@/components/agent';
-import { getRandomInterviewCover } from '@/lib/utils';
-
 import { getCurrentUser } from '@/lib/actions/auth.action';
 
-const InterviewDetails = async ({ params }: RouteParams) => {
-  const { id } = await params;
-
+const Page = async () => {
   const user = await getCurrentUser();
 
-  if (!user) redirect('/sign-in');
+  if (!user) {
+    return null;
+  }
 
   return (
     <>
-      <div className='flex flex-row gap-4 justify-between'>
-        <div className='flex flex-row gap-4 items-center max-sm:flex-col'>
-          <div className='flex flex-row gap-4 items-center'>
-            <Image
-              src={getRandomInterviewCover()}
-              alt='cover-image'
-              width={40}
-              height={40}
-              className='rounded-full object-cover size-[40px]'
-            />
-            {/* <h3 className='capitalize'>{interview.role} Interview</h3> */}
-          </div>
-
-          {/* <TechIcons techStack={interview.techstack} /> */}
-        </div>
-
-        <p className='bg-dark-200 px-4 py-2 rounded-lg h-fit'>
-          Interview ID: {id}
-        </p>
-      </div>
+      <h3>Interview generation</h3>
 
       <Agent
-        userName={'John Doe'}
+        userName={user.name}
         userId={user?.id}
-        interviewId={id}
-        type='interview'
-        // questions={interview.questions}
-        // feedbackId={feedback?.id}
+        profileImage={''}
+        type='generate'
       />
     </>
   );
 };
 
-export default InterviewDetails;
+export default Page;
